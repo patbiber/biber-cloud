e58dfdfe5432fc34bb8406031e784c8413c69841
Patrick Biber <p.biber@southpole.com>
2024-01-12 18:26:58 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scroll for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Form validation logic (to be added)
